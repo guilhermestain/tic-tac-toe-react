531a21e745f2f7364c245c2bb9632cb79b669e00
Guilherme Stain <guilhermestain@gmail.com>
2019-01-17 10:00:24 -0200

    react-game

--- src/components/Square/index.js
+++ src/components/Square/index.js
@@ -1,32 +1,13 @@
-import './index.css'
-class Board extends React.Component {
-    renderSquare(i) {
-      return <Square />;
-    }
-  
-    render() {
-      const status = 'Next player: X';
-  
-      return (
-        <div>
-          <div className="status">{status}</div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
-        </div>
-      );
-    }
-  }
-  export default Board
+import React, {Component} from 'react'
+import './index.css' 
+
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
+
+
+export default Square
